eac022ceb01bfc7297048ea5a958a2ba41fb61a5
Denis Anfruns <anfruns.denis@gmail.com>
2023-03-23 02:18:34 +0100

    Add Converter 14th Project

---
+++ 14.Converter.MAUI/Converter.MAUI/MVVM/Views/ConverterView.xaml.cs
@@ -0,0 +1,18 @@
+using Converter.MAUI.MVVM.ViewModels;
+
+namespace Converter.MAUI.MVVM.Views;
+
+public partial class ConverterView : ContentPage
+{
+	public ConverterView()
+	{
+		InitializeComponent();
+		//BindingContext = new ConverterViewModel();
+	}
+
+	private void Picker_SelectedIndexChanged(object sender, EventArgs e)
+	{
+		var viewModel = (ConverterViewModel)BindingContext;
+		viewModel.Convert();
+    }
+}
---
+++ 14.Converter.MAUI/Converter.MAUI/README.md
@@ -0,0 +1,5 @@
+#### MAUIVERTER
+
+<img src="/Screenshots/MAUIVerter0.png" width="250">
+<img src="/Screenshots/MAUIVerter1.png" width="250">
+<img src="/Screenshots/MAUIVerter2.png" width="250">
