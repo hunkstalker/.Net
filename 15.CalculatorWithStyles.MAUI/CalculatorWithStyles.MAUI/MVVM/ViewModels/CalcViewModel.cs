842253b2bf7b06c2631c9236f4b2b7fb0a8b3813
Denis Anfruns <anfruns.denis@gmail.com>
2023-04-24 01:13:35 +0200

    Add Finished project CalculatorWithStyles

--- 15.CalculatorWithStyles.MAUI/CalculatorWithStyles.MAUI/MVVM/ViewModels/CalcViewModel.cs
+++ 15.CalculatorWithStyles.MAUI/CalculatorWithStyles.MAUI/MVVM/ViewModels/CalcViewModel.cs
@@ -1,4 +1,5 @@
-﻿using PropertyChanged;
+﻿using Dangl.Calculator;
+using PropertyChanged;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,5 +16,22 @@ namespace CalculatorWithStyles.MAUI.MVVM.ViewModels
         public string Result { get; set; } = "0";
 
         public ICommand OperationCommand => new Command(number => { Formula += number; });
-    }
+        public ICommand ResetCommand => new Command(() =>
+        {
+            Result = "0";
+            Formula = "";
+        });
+		public ICommand BackspaceCommand => new Command(number => {
+            if (Formula.Length > 0)
+            {
+                Formula = Formula.Substring(0, Formula.Length - 1);
+            }
+        });
+        public ICommand CalculateComand => new Command(() =>
+        {
+            if (Formula.Length == 0) return;
+            var calculation = Calculator.Calculate(Formula.Replace('x', '*'));
+            Result = calculation.Result.ToString();
+        });
+	}
 }
---
+++ 15.CalculatorWithStyles.MAUI/README.md
@@ -0,0 +1,3 @@
+#### MAUI CALCULATOR WITH STYLES
+
+<img src='/Screenshots/Calculator.MAUI_DarkLightModes.gif' width='300'> <img src='/Screenshots/Calculator.MAui_DarkMode.png' width='300'>
