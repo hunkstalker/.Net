ebb3e2e8ea9eaca0b23cfc9760cd47d3c0e5227f
Denis Anfruns <anfruns.denis@gmail.com>
2023-05-06 20:09:45 +0200

    Add 151.Detecting scrolling

--- 16.MasteringCollectionView.MAUI/MasteringCollectionView.MAUI/MVVM/Views/ProductsView.xaml.cs
+++ 16.MasteringCollectionView.MAUI/MasteringCollectionView.MAUI/MVVM/Views/ProductsView.xaml.cs
@@ -1,4 +1,5 @@
 using MasteringCollectionView.MAUI.MVVM.ViewModels;
+using System.Diagnostics;
 
 namespace MasteringCollectionView.MAUI.MVVM.Views;
 
@@ -8,5 +9,18 @@ public partial class ProductsView : ContentPage
 	{
 		InitializeComponent();
 		BindingContext = new ProductsViewModel();
+	}
+
+	private void CollectionView_Scrolled(object sender, ItemsViewScrolledEventArgs e)
+	{
+		Debug.Write("--------------------");
+		Debug.WriteLine("HorizontalDelta: " + e.HorizontalDelta);
+		Debug.WriteLine("VerticalDelta: " + e.VerticalDelta);
+		Debug.WriteLine("HorizontalOffset: " + e.HorizontalOffset);
+		Debug.WriteLine("VerticalOffset: " + e.VerticalOffset);
+		Debug.WriteLine("FirstVisibleItemIndex: " + e.FirstVisibleItemIndex);
+		Debug.WriteLine("CenterItemIndex: " + e.CenterItemIndex);
+		Debug.WriteLine("LastVisibleItemIndex: " + e.LastVisibleItemIndex);
+		Debug.Write("--------------------");
 	}
 }
