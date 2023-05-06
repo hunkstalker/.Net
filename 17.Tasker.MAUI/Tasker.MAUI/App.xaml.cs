7bbe3b91ff335592c2490148c7218b11a73f0654
Denis Anfruns <anfruns.denis@gmail.com>
2023-05-07 01:47:34 +0200

    Initial commit Tasker

---
+++ 17.Tasker.MAUI/Tasker.MAUI/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace Tasker.MAUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+
+	protected override Window CreateWindow(IActivationState activationState)
+	{
+		Window window = base.CreateWindow(activationState);
+
+		window.Height = 760;
+		window.Width = 420;
+
+		return window;
+	}
+}
