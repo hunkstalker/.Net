78807f2bdb2dbec8c54a09b198d8ac6eec4c474a
Denis Anfruns <anfruns.denis@gmail.com>
2023-05-08 00:21:21 +0200

    Add 169.Cretaing the HTTPClient object

---
+++ 18.RESTDemo.MAUI/RESTDemo.MAUI/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace RESTDemo.MAUI
+{
+	public class MainViewModel
+	{
+		HttpClient client;
+		JsonSerializerOptions _serializerOptions;
+
+		public MainViewModel()
+		{
+			client = new HttpClient();
+			_serializerOptions = new JsonSerializerOptions
+			{
+				WriteIndented = true,
+			};
+		}
+
+		public ICommand AddUserCommand => new Command(() =>
+		{
+
+		});
+	}
+}
