136ffb4e3ae7b158ee523eecba2b8ba21796298f
Denis Anfruns <anfruns.denis@gmail.com>
2023-05-07 05:10:04 +0200

    Add 161.Creating the categories section 2

--- 17.Tasker.MAUI/Tasker.MAUI/MVVM/ViewModels/MainViewModel.cs
+++ 17.Tasker.MAUI/Tasker.MAUI/MVVM/ViewModels/MainViewModel.cs
@@ -11,6 +11,7 @@ namespace Tasker.MAUI.MVVM.ViewModels
 	public class MainViewModel
 	{
 		public ObservableCollection<Category> Categories { get; set; }
+		public ObservableCollection<MyTask> Tasks { get; set; }
 
 		public MainViewModel()
 		{
@@ -23,23 +24,97 @@ namespace Tasker.MAUI.MVVM.ViewModels
 			{
 				new Category
 				{
-					Id = 1,
-					CategoryName = ".Net MAUI Course",
-					Color = "#CF14DF"
+						Id = 1,
+						CategoryName = ".NET MAUI Course",
+						Color = "#CF14DF"
 				},
 				new Category
 				{
-					Id = 2,
-					CategoryName = "Tutorials",
-					Color = "#DF6F14"
+						Id = 2,
+						CategoryName = "Tutorials",
+						Color = "#df6f14"
 				},
 				new Category
 				{
-					Id = 3,
-					CategoryName = "Shopping",
-					Color = "#14DF80"
+						Id = 3,
+						CategoryName = "Shopping",
+						Color = "#14df80"
 				}
 			};
+			Tasks = new()
+			{
+				new MyTask
+				{
+						TaskName = "Upload exercise files",
+						Completed = false,
+						CategoryId = 1
+				},
+				new MyTask
+				{
+						TaskName = "Plan next course",
+						Completed = false,
+						CategoryId = 1
+				},
+				new MyTask
+				{
+						TaskName = "Upload new ASP.NET video on YouTube",
+						Completed = false,
+						CategoryId = 2
+				},
+				new MyTask
+				{
+						TaskName = "Fix Settings.cs class of the project",
+						Completed = false,
+						CategoryId = 2
+				},
+				new MyTask
+				{
+						TaskName = "Update github repository",
+						Completed = true,
+						CategoryId = 2
+				},
+				new MyTask
+				{
+						TaskName = "Buy eggs",
+						Completed = false,
+						CategoryId = 3
+				},
+				new MyTask
+				{
+						TaskName = "Go for the pepperoni pizza",
+						Completed = false,
+						CategoryId = 3
+				},
+			};
+			UpdateData();
+		}
+		
+		public void UpdateData()
+		{
+			foreach(var c in Categories)
+			{
+				var tasks = from t in Tasks
+							where t.CategoryId == c.Id
+							select t;
+
+				var completed = from t in tasks
+							where t.Completed == true
+							select t;
+
+				var notCompleted = from t in tasks
+							where t.Completed == false
+							select t;
+
+				c.PendingTasks = notCompleted.Count();
+				c.Percentage = (float)completed.Count()/(float)tasks.Count();
+			}
+			foreach(var t in Tasks)
+			{
+				var catColor = (from c in Categories
+							   where c.Id == t.CategoryId
+							   select c.Color).FirstOrDefault();
+				t.TaskColor = catColor;
+			}
 		}
 	}
 }
