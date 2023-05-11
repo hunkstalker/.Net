9b793fbfca96fdf7671dc6a214fe8e5ab8cdd963
Denis Anfruns <anfruns.denis@gmail.com>
2023-05-11 02:39:56 +0200

    Add 182.Getting the coordinates from a location

--- 19.Weather.MAUI/Weather.MAUI/MVVM/ViewModels/WeatherViewModel.cs
+++ 19.Weather.MAUI/Weather.MAUI/MVVM/ViewModels/WeatherViewModel.cs
@@ -1,13 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Input;
 
 namespace Weather.MAUI.MVVM.ViewModels
 {
-	internal class WeatherViewModel
+	public class WeatherViewModel
 	{
+		public ICommand SearchCommand => new Command(async (searchText) =>
+		{
+			var location = await GetCoordinatesAsync(searchText.ToString());
+		});
 
+		public static async Task<Location> GetCoordinatesAsync(string address)
+		{
+			IEnumerable<Location> locations = await Geocoding.Default.GetLocationsAsync(address);
+
+			Location location = locations?.FirstOrDefault();
+
+			if (location != null)
+				Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+			return location;
+		}
 	}
 }
