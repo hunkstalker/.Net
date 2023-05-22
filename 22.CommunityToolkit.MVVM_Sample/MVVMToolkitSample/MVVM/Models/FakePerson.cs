8fc91170fa2c607a75f7d91d21e31104c55229d7
Denis Anfruns <anfruns.denis@gmail.com>
2023-05-23 00:29:29 +0200

    Add CommunityToolkit.MVVM sample

---
+++ 22.CommunityToolkit.MVVM_Sample/MVVMToolkitSample/MVVM/Models/FakePerson.cs
@@ -0,0 +1,27 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MVVMToolkitSample.MVVM.Models
+{
+     public partial class FakePerson : ObservableObject
+     {
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(FullName))]
+		private string _firstName;
+		[ObservableProperty]
+		private string _lastName;
+
+		partial void OnFirstNameChanged(string value)
+		{
+			Console.WriteLine(value);
+		}
+
+		public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+     }
+}
